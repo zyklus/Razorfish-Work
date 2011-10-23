ad3057799e462208b06703628da170537e3b2c57
Mark Kahn <cwolves@gmail.com>
2011-10-23 10:30:08 -0700

    Updating ios prototype

--- disney-ios-prototype/js/demo.js
+++ disney-ios-prototype/js/demo.js
@@ -1,15 +1,28 @@
 $( window ).bind( 'load', function(){
 	var $scroller = $( '#scroller' )
+	  ,  $wrapper = $( '#wrapper' )
+	  ,     $body = $( document.body )
 	  ,     $img1 = $scroller.find( 'img' )
 	  ,     $img2 = $img1.clone().appendTo( $scroller )
 	  ,  imgWidth = $img1.width()
 	  , mouseInfo = false
-	  ,   leftPos = 0;
-;
-	$( document.body )
+	  ,   leftPos = 0
+	  ,  lastLeft = 0;
+
+	// prevent slight gap between the two images when tilted (remove if tilt is removed)
+	$img2.css({ position : 'relative', left : -1 })
+
+	$body
 		.bind( 'mousedown touchstart', mouseDown )
 		.bind( 'mouseup touchend'    , mouseUp   )
-		.bind( 'mousemove touchmove' , mouseMove );
+		.bind( 'mousemove touchmove' , mouseMove )
+
+	$wrapper.css ( 'webkitTransition', 'all 0.3s ease-in-out' );
+	onRotate();
+
+	function onRotate(){
+		$wrapper.css( '-webkit-transform-origin', ( $wrapper.width() >> 1 ) + 'px ' + ( $wrapper.height() >> 1 ) + 'px' );
+	}
 
 	function mouseDown( ev ){
 		mouseInfo = { pX: ev.pageX, pY: ev.pageY, leftPos: leftPos };
@@ -20,6 +33,8 @@ $( window ).bind( 'load', function(){
 	function mouseUp( ev ){
 		mouseInfo = false;
 		ev.preventDefault();
+
+		leftPos = lastLeft;
 	}
 
 	function mouseMove( ev ){
@@ -29,10 +44,23 @@ $( window ).bind( 'load', function(){
 		setXPos( leftPos + ev.pageX - mouseInfo.pX );
 	}
 
-	function setXPos( pos ){
-		while( pos < 0 )        pos += imgWidth;
-		while( pos > imgWidth ) pos -= imgWidth;
+	window.addEventListener( 'deviceorientation', function( ev ){
+		var heading = ev.webkitCompassHeading;
+		if( !heading ){ return; }
 
-		$scroller.scrollLeft( leftPos = pos );
+		setXPos( heading / 360 * imgWidth );
+	} );
+
+	window.addEventListener( 'deviceorientation', function( ev ) {
+//		$wrapper.css( 'webkitTransform', 'rotate(' + -ev.gamma + 'deg)' );
+	} );
+
+	function setXPos( pos ){
+		while( pos < -imgWidth ) pos += imgWidth;
+		while( pos > 0         ) pos -= imgWidth;
+
+		pos = pos;
+
+		$scroller.css({ left: lastLeft = pos });
 	}
 });
